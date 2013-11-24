5ba7eae0e1fcc35f29d811b9282e88353ab7ebd2
leodufer <leodufer@gmail.com>
2013-11-23 22:41:02 -0300

    añadido slider menu

--- gen/py/edu/fpune/tfg/turistaapp/R.java
+++ gen/py/edu/fpune/tfg/turistaapp/R.java
@@ -8,7 +8,26 @@
 package py.edu.fpune.tfg.turistaapp;
 
 public final class R {
+    public static final class array {
+        /**  Nav Drawer List Item Icons 
+ Keep them in order as the titles are in 
+         */
+        public static final int nav_drawer_icons=0x7f070001;
+        /**  Nav Drawer Menu Items 
+         */
+        public static final int nav_drawer_items=0x7f070000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int counter_text_bg=0x7f040004;
+        public static final int counter_text_color=0x7f040005;
+        public static final int list_background=0x7f040001;
+        public static final int list_background_pressed=0x7f040002;
+        public static final int list_divider=0x7f040003;
+        public static final int list_item_title=0x7f040000;
+        public static final int pressed_styleturista=0x7f040006;
+        public static final int verde=0x7f040007;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,27 +36,95 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ab_background_textured_styleturista=0x7f020000;
+        public static final int ab_bottom_solid_styleturista=0x7f020001;
+        public static final int ab_solid_styleturista=0x7f020002;
+        public static final int ab_stacked_solid_styleturista=0x7f020003;
+        public static final int ab_texture_tile_styleturista=0x7f020004;
+        public static final int ab_transparent_styleturista=0x7f020005;
+        public static final int btn_cab_done_default_styleturista=0x7f020006;
+        public static final int btn_cab_done_focused_styleturista=0x7f020007;
+        public static final int btn_cab_done_pressed_styleturista=0x7f020008;
+        public static final int btn_cab_done_styleturista=0x7f020009;
+        public static final int cab_background_bottom_styleturista=0x7f02000a;
+        public static final int cab_background_top_styleturista=0x7f02000b;
+        public static final int counter_bg=0x7f02000c;
+        public static final int ic_communities=0x7f02000d;
+        public static final int ic_drawer=0x7f02000e;
+        public static final int ic_home=0x7f02000f;
+        public static final int ic_hotel=0x7f020010;
+        public static final int ic_launcher=0x7f020011;
+        public static final int ic_pages=0x7f020012;
+        public static final int ic_people=0x7f020013;
+        public static final int ic_photos=0x7f020014;
+        public static final int ic_restaurant=0x7f020015;
+        public static final int ic_whats_hot=0x7f020016;
+        public static final int list_focused_styleturista=0x7f020017;
+        public static final int list_item_bg_normal=0x7f020018;
+        public static final int list_item_bg_pressed=0x7f020019;
+        public static final int list_selector=0x7f02001a;
+        public static final int menu_dropdown_panel_styleturista=0x7f02001b;
+        public static final int pressed_background_styleturista=0x7f02001c;
+        public static final int progress_bg_styleturista=0x7f02001d;
+        public static final int progress_horizontal_styleturista=0x7f02001e;
+        public static final int progress_primary_styleturista=0x7f02001f;
+        public static final int progress_secondary_styleturista=0x7f020020;
+        public static final int selectable_background_styleturista=0x7f020021;
+        public static final int spinner_ab_default_styleturista=0x7f020022;
+        public static final int spinner_ab_disabled_styleturista=0x7f020023;
+        public static final int spinner_ab_focused_styleturista=0x7f020024;
+        public static final int spinner_ab_pressed_styleturista=0x7f020025;
+        public static final int spinner_background_ab_styleturista=0x7f020026;
+        public static final int tab_indicator_ab_styleturista=0x7f020027;
+        public static final int tab_selected_focused_styleturista=0x7f020028;
+        public static final int tab_selected_pressed_styleturista=0x7f020029;
+        public static final int tab_selected_styleturista=0x7f02002a;
+        public static final int tab_unselected_focused_styleturista=0x7f02002b;
+        public static final int tab_unselected_pressed_styleturista=0x7f02002c;
+        public static final int tab_unselected_styleturista=0x7f02002d;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f0a0007;
+        public static final int counter=0x7f0a0005;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int frame_container=0x7f0a0001;
+        public static final int icon=0x7f0a0003;
+        public static final int list_slidermenu=0x7f0a0002;
+        public static final int title=0x7f0a0004;
+        public static final int txtLabel=0x7f0a0006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int drawer_list_item=0x7f030001;
+        public static final int fragment_community=0x7f030002;
+        public static final int fragment_find_people=0x7f030003;
+        public static final int fragment_home=0x7f030004;
+        public static final int fragment_pages=0x7f030005;
+        public static final int fragment_photos=0x7f030006;
+        public static final int fragment_whats_hot=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        /**  Content Description 
+         */
+        public static final int desc_list_item_icon=0x7f060005;
+        public static final int drawer_close=0x7f060004;
+        public static final int drawer_open=0x7f060003;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Styleturista=0x7f080003;
+        public static final int ActionBar_Transparent_Styleturista=0x7f080004;
+        public static final int ActionBarTabStyle_Styleturista=0x7f080007;
+        public static final int ActionButton_CloseMode_Styleturista=0x7f08000a;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -59,10 +146,18 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+        public static final int DropDownListView_Styleturista=0x7f080006;
+        public static final int DropDownNav_Styleturista=0x7f080008;
+        public static final int PopupMenu_Styleturista=0x7f080005;
+        public static final int ProgressBar_Styleturista=0x7f080009;
+        public static final int Theme_Styleturista=0x7f080002;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Styleturista_Widget=0x7f08000b;
     }
 }
